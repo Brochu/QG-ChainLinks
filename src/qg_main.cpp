780d086cd03a1823eb435fba12440c68a7f70abf
Brochu <alexbrochu1@gmail.com>
2025-10-10 15:25:47 -0400

    [SETUP] SDL3 tests

--- src/qg_main.cpp
+++ src/qg_main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL3/SDL.h"
 
 int main(int argc, char **argv) {
+    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
+        printf("Could not init SDL3\nerror: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
     printf("[ALIBI] My own starting point!\n");
+
+    SDL_Quit();
     return 0;
 }
