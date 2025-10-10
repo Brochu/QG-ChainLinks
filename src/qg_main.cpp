785f8b2de101e37a5930305e97d2037213c3dcc7
Brochu <alexbrochu1@gmail.com>
2025-10-10 16:03:58 -0400

    [DEV] Trying basics of SDL3

--- src/qg_main.cpp
+++ src/qg_main.cpp
@@ -3,14 +3,54 @@
 
 #include "SDL3/SDL.h"
 
+#define VERSION "ALPHA" //TODO: Export version from the main game DLL
+#define TITLE "QG-ChainLinks"
+#define WIDTH 800
+#define HEIGHT 600
+
+bool qg_running = true;
+float pos = 0.f;
+
 int main(int argc, char **argv) {
     if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
         printf("Could not init SDL3\nerror: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
+    printf("[QG] opening SDL3 window!\n");
 
-    printf("[ALIBI] My own starting point!\n");
+    SDL_Window *window;
+    SDL_Renderer *context;
+    if (!SDL_CreateWindowAndRenderer("[" VERSION "]" TITLE, 800, 600, 0, &window, &context)) {
+        printf("Could not create Window or Renderer\nerror: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
+    SDL_Event event;
+    while (qg_running) {
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                qg_running = false;
+            }
+        }
+
+        // Update State
+        pos += 0.01f;
+
+        // Draw current frame
+        SDL_SetRenderDrawColor(context, 255, 255, 255, 255);
+        SDL_RenderFillRect(context, NULL);
+
+        SDL_SetRenderDrawColor(context, 255, 0, 0, 255);
+        SDL_FRect r;
+        r.x = r.y = pos;
+        r.w = r.h = 100.f;
+        SDL_RenderFillRect(context, &r);
+
+        SDL_RenderPresent(context);
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
