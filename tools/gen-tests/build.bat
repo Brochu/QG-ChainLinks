daa98c96113366d846bfc068fc5d42efdb9ec882
Brochu <alexbrochu1@gmail.com>
2025-10-17 16:05:19 -0400

    [SETUP] Integrate w/ SQLite3, store case generation data. Great for debug

--- tools/gen-tests/build.bat
+++ tools/gen-tests/build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cl -nologo -c -std:c++17 -EHsc -MD -TP -Zi ./gen-main.cpp
-link -nologo gen-main.obj -out:app.exe
+cl -nologo -c -std:c++17 -EHsc -MD -TP -Zi -I../../include/ ./gen-main.cpp
+link -nologo gen-main.obj ../../libs/sqlite3.lib -out:app.exe
--- tools/gen-tests/gen-main.cpp
+++ tools/gen-tests/gen-main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdint>
 
+#include "sqlite3.h"
+
 enum class loc_type_t : int32_t {
     LOCATION_NONE,
     LOCATION_RESTAURANT,
@@ -53,9 +55,26 @@ struct gen_event_t {
 static gen_event_t s_events[1024];
 static size_t s_num_events = 0;
 
+static int debug_callback(void *ptr, int n, char **text0, char **text1) {
+    return 0;
+}
+
 //TODO: Define the steps to generate cases
 int main(int argc, char **argv) {
     printf("[PROC-GEN] Testing history/events generation & simulation\n");
     s_events[s_num_events++] = { 0 };
+
+    sqlite3 *db;
+    int res = sqlite3_open("./detective.db", &db);
+    printf("[PROC-GEN] Opening Case DB; res = %i\n", res);
+
+    char *err = nullptr;
+    res = sqlite3_exec(db, "SELECT * FROM users;", debug_callback, NULL, &err);
+    printf("[PROC-GEN] EXEC res = %i; err = %s\n", res, err);
+
+    res = sqlite3_close_v2(db);
+    printf("[PROC-GEN] Closing Case DB; res = %i\n", res);
+    printf("[PROC-GEN] Just testing, SQLITE_OK value = %i\n", SQLITE_OK);
+
     return 0;
 }
