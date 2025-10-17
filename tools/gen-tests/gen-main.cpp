ad433be271959219a221df4ef9f11cadc3d8d442
Brochu <alexbrochu1@gmail.com>
2025-10-16 20:06:37 -0400

    [DEV] Started working on structs needed for proc gen of cases

--- tools/gen-tests/gen-main.cpp
+++ tools/gen-tests/gen-main.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 #include <cstdint>
 
+struct gen_loc_t {
+    int64_t id;
+};
+
 struct gen_traits_t {
     int32_t greed;
     int32_t aggro;
@@ -16,11 +20,30 @@ struct gen_char_t {
     gen_traits_t traits;
 };
 
+enum class gen_type_t : int32_t {
+    EVENT_NONE,
+    EVENT_CRIME,
+    EVENT_SIGHTING,
+    EVENT_COUNT,
+};
+
 struct gen_event_t {
     int64_t id;
-};
-//TODO: Find out to represent a graph of events w/ temporal dependencies
+    int64_t sim_step;
+    int64_t next_idx[8] = { 0 };
+    size_t num_next = 0;
 
+    gen_type_t type;
+    union {
+        struct {
+        } crime;
+        struct {
+        } sighting;
+    };
+};
+static gen_event_t s_events[1024];
+
+//TODO: Define the steps to generate cases
 int main(int argc, char **argv) {
     printf("[PROC-GEN] Testing history/events generation & simulation\n");
     return 0;
