add2b8d014c8361c7b2a00a29850cb0c4df9e607
Alex Brochu <alexbrochu1@gmail.com>
2025-11-12 23:17:20 -0800

    [DEV] Make sure we build in debug mode for now

--- tools/gen-tests/build.bat
+++ tools/gen-tests/build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 cl -nologo -c -std:c++17 -EHsc -MD -TP -Zi -I../../include/ ./gen-main.cpp
-link -nologo gen-main.obj ../../libs/sqlite3.lib -out:app.exe
+link -nologo gen-main.obj ../../libs/sqlite3.lib -debug -out:app.exe
