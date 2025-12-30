cb6da68d30a9efac7857556f9e12eaf56afb6451
Alex Brochu <alexbrochu1@gmail.com>
2025-12-30 10:58:34 -0800

    [DEV] First pass on dynamic game library loading

--- src/qg_main.cpp
+++ src/qg_main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_render.h"
@@ -8,6 +9,58 @@
 #include "qg_config.hpp"
 #include "qg_generator.hpp"
 #include "qg_random.hpp"
+
+// ------------- GAMELIB LOADING
+
+#define GAMELIB_BASE_PATH "./gamelibs/chain.dll"
+#define GAMELIB_LOAD_PATH "./gamelibs/chain_loaded.dll"
+
+#define GAMELIB_FUNCS \
+    X(void, game_init, "chain_init", (void)) \
+    X(void, game_tick, "chain_tick", (float)) \
+    X(void, game_draw, "chain_draw", (float)) \
+    X(void, game_close, "chain_exit", (void))
+
+#define X(ret, name, sym, args) typedef ret (*name##_t) args;
+GAMELIB_FUNCS
+#undef X
+
+#define X(ret, name, sym, args) name##_t name = NULL;
+GAMELIB_FUNCS
+#undef X
+
+HMODULE gamelib_mod = NULL;
+
+void gamelib_load() {
+    assert(gamelib_mod == NULL && "Did not properly free the gamelib module");
+
+    if (GetFileAttributes(GAMELIB_LOAD_PATH) == INVALID_FILE_ATTRIBUTES) {
+        CopyFile(GAMELIB_BASE_PATH, GAMELIB_LOAD_PATH, false);
+    }
+    gamelib_mod = LoadLibrary(GAMELIB_LOAD_PATH);
+    assert(gamelib_mod != NULL && "Failed to load game library");
+
+    #define X(ret, name, sym, args) \
+        name = (name##_t)GetProcAddress(gamelib_mod, sym); \
+        assert(name != NULL && "Failed to load function " sym);
+    GAMELIB_FUNCS
+    #undef X
+}
+
+void gamelib_free() {
+    if (gamelib_mod != NULL) {
+        FreeLibrary(gamelib_mod);
+        gamelib_mod = NULL;
+    }
+}
+
+void gamelib_refresh() {
+    gamelib_free();
+    CopyFile(GAMELIB_BASE_PATH, GAMELIB_LOAD_PATH, false);
+    gamelib_load();
+}
+
+// ------------- ENGINE - MAIN
 
 #define VERSION "ALPHA" //TODO: Export version from the main game DLL
 #define TITLE "QG-ChainLinks"
@@ -26,6 +79,9 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
     printf("[QG] SDL3 Correctly init'ed!\n");
+    //gamelib_load();
+
+    game_init();
 
     case_gen ctx = {};
     case_gen_init(&ctx);
@@ -33,6 +89,7 @@ int main(int argc, char **argv) {
     case_gen_population(&ctx);
     case_gen_clear(&ctx);
 
+    //gamelib_free();
     SDL_Quit();
     printf("[QG] quitting SDL3!\n");
     return 0;
