433e6955f7d6a658c99dd010ed7180a5e7fd1247
Alex Brochu <alexbrochu1@gmail.com>
2025-12-30 09:47:39 -0800

    [DEV] Added possibility to allocate on memory arena with offsets instead of ptrs
    The goal here is to be able to track allocations with offsets instead of
    pointers for cases where I need to allocate from the game DLL and
    reconstruct objects from the arena after a hot-reload

--- include/qg_memory.hpp
+++ include/qg_memory.hpp
@@ -2,6 +2,8 @@
 
 #include "qg_types.hpp"
 
+#include <cassert>
+
 void *qg_malloc(u64 sz);
 void *qg_calloc(u64 count, u64 sz);
 void *qg_realloc(void *ptr, u64 sz);
@@ -19,7 +21,20 @@ struct arena_ptr {
     u64 gen;
 };
 
+struct arena_off {
+    u64 off;
+    u64 gen;
+};
+
 void mem_arena_init(mem_arena *arena, u64 max_size);
-arena_ptr mem_arena_alloc(mem_arena *arena, u64 size, u64 align = sizeof(void *));
 void mem_arena_reset(mem_arena *arena);
 void mem_arena_clear(mem_arena *arena);
+
+arena_ptr mem_arena_alloc(mem_arena *arena, u64 size, u64 align = sizeof(void *));
+arena_off mem_arena_offloc(mem_arena *arena, u64 size, u64 align = sizeof(void *));
+
+template<class T>
+static inline T *mem_arena_at(mem_arena *arena, arena_off offset) {
+    assert(arena->gen == offset.gen && "Trying to access stale offset in arena");
+    return reinterpret_cast<T *>(arena->base + offset.off);
+}
--- src/qg_mem.cpp
+++ src/qg_mem.cpp
@@ -1,5 +1,4 @@
 #include "qg_memory.hpp"
-#include <cassert>
 #include <cstdlib>
 
 //TODO: Add memory tracking w/ profiler? Tracy
@@ -37,18 +36,6 @@ void mem_arena_init(mem_arena *arena, u64 max_size) {
     arena->cap = max_size;
 }
 
-arena_ptr mem_arena_alloc(mem_arena *arena, u64 size, u64 align) {
-    u64 off = align_fwd(arena->next, align);
-
-    if (off + size > arena->cap) {
-        assert(false && "ASSERT: mem_arena ran out of allocated memory");
-        return { nullptr, arena->gen };
-    }
-    u8 *ptr = arena->base + off;
-    arena->next = off + size;
-    return { ptr, arena->gen };
-}
-
 void mem_arena_reset(mem_arena *arena) {
     arena->next = 0;
     arena->gen++;
@@ -61,4 +48,28 @@ void mem_arena_clear(mem_arena *arena) {
     arena->next = 0;
     arena->cap = 0;
     arena->gen++;
+}
+
+arena_ptr mem_arena_alloc(mem_arena *arena, u64 size, u64 align) {
+    u64 off = align_fwd(arena->next, align);
+
+    if (off + size > arena->cap) {
+        assert(false && "ASSERT: mem_arena ran out of allocated memory");
+        return { nullptr, arena->gen };
+    }
+    u8 *ptr = arena->base + off;
+    arena->next = off + size;
+    return { ptr, arena->gen };
+}
+
+arena_off mem_arena_offloc(mem_arena *arena, u64 size, u64 align) {
+    u64 off = align_fwd(arena->next, align);
+
+    if (off + size > arena->cap) {
+        assert(false && "ASSERT: mem_arena ran out of allocated memory");
+        return { UINT64_MAX, arena->gen }; //TODO: This is dangerous, need to rethink invalid offsets
+    }
+
+    arena->next = off + size;
+    return { off, arena->gen };
 }
