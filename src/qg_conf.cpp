7213d46d99b3a1012b7e1cd4fbc7741b5101b2d3
Alex Brochu <alexbrochu1@gmail.com>
2026-01-04 23:57:23 -0800

    [DEV] WIP: Start working on dynamic runtime config system; loading the ini files

--- src/qg_conf.cpp
+++ src/qg_conf.cpp
@@ -1,6 +1,24 @@
 #include "qg_config.hpp"
 
+#include "SDL3/SDL.h"
+#include <cstdio>
+
 void config_init(config *c, const char *file) {
+    u64 file_size = 0;
+    char *content = (char *)SDL_LoadFile(file, &file_size);
+
+    char *next = NULL;
+    char *token = strtok_s(content, "\r\n", &next);
+    while (token) {
+        printf("%s\n", token);
+        token = strtok_s(NULL, "\r\n", &next);
+
+        //TODO: Parse line by line
+        // Split at '='
+        // detect CSVs; split at ',' if the case
+    }
+
+    SDL_free(content);
 }
 
 void config_free(config *c) {
