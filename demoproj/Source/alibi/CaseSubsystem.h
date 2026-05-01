0156e49532cfd034f2d97741986a3280204d2212
Alex Brochu <alexbrochu1@gmail.com>
2026-04-30 21:34:33 -0700

    Added intent with the CaseSubsystem - should start reading case files shortly
    Still need to finalize a workable version 0 for case files...

--- demoproj/Source/alibi/CaseSubsystem.h
+++ demoproj/Source/alibi/CaseSubsystem.h
@@ -6,11 +6,23 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "CaseSubsystem.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEvidenceFound, FString, evidence_name);
+USTRUCT()
+struct FLabResults {
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEvidenceFound, int, evidence_idx);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParam(FOnLabRequestComplete, int, evidence_idx, FLabResults, results);
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTimeBudgetChanged, int, from, int, to);
 
 /**
- * 
+ * Subsystem that will keep track of the current case metadata, resources, player actions on the world.
+ * Will store the current status of the case's world.
+ * The actions the player take will be reported and handled here
+ *   - examine location/evidence
+ *   - (start/stop) witness interview
+ *   - request study of evidence
+ *   - combine info/evidence together
  */
 UCLASS()
 class ALIBI_API UCaseSubsystem : public UGameInstanceSubsystem
