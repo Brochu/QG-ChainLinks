302d112430d70c9fa4b6e204ada993fa4ccbded9
Alex Brochu <alexbrochu1@gmail.com>
2026-06-28 08:25:52 -0700

    started initial work, parsing v0 case file format
    Text based file format for now, will move onto binary format when
    everything is ironed out

--- demoproj/Source/alibi/CaseSubsystem.cpp
+++ demoproj/Source/alibi/CaseSubsystem.cpp
@@ -2,17 +2,46 @@
 
 #include "CaseSubsystem.h"
 
-void parse_metadata(TArray<FString> lines) {
+DEFINE_LOG_CATEGORY(LogCase);
+
+void parse_metadata(FCaseData &c, const TArray<FString> &lines, size_t &offset) {
+	while (!lines[offset].Contains("[case]")) {
+		offset++;
+	}
+	offset++;
+
+	while (!lines[offset].IsEmpty()) {
+		FString key;
+		FString val;
+		if (lines[offset].Split("=", &key, &val)) {
+			// log error
+		}
+		key.TrimStartAndEndInline();
+		val.TrimStartAndEndInline();
+
+		UE_LOG(LogCase, Log, TEXT("[CASE PARSE] - {'%s' ; '%s'}"), *key, *val);
+		offset++;
+	}
 }
-void parse_location(TArray<FString> lines) {
+
+void parse_location(FCaseData &c, TArray<FString> &lines, size_t &offset) {
 }
-void parse_action(TArray<FString> lines) {
+void parse_action(FCaseData &c, TArray<FString> &lines, size_t &offset) {
 }
 
 void UCaseSubsystem::load_case_file(FString case_path) {
-	current_case.name = FText::FromString("Case #1");
-	current_case.desc = FText::FromString("Case - Lorem Ipsum");
-	current_case.intro_knot = TEXT("nil");
+	TArray<FString> file;
+	if (!FFileHelper::LoadFileToStringArray(file, *case_path)) {
+		// loggin error here
+		return;
+	}
+
+	current_case.name = FText::FromString("");
+	current_case.desc = FText::FromString("");
+	current_case.intro_knot = "";
 	current_case.version = 0;
+	size_t position = 0;
+	parse_metadata(current_case, file, position);
+
 	//TODO: Parse file at case_path here
 }
--- demoproj/Source/alibi/CaseSubsystem.h
+++ demoproj/Source/alibi/CaseSubsystem.h
@@ -5,54 +5,9 @@
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "AlibiEntityDefinitions.h"
+#include "ChainSubsystem.h"
 #include "MatrixSubsystem.h"
 #include "CaseSubsystem.generated.h"
-
-UENUM(BlueprintType)
-enum class EEvidenceType : uint8 { EVI_TESTIMONY, EVI_PHYSICAL_TRACE, EVI_RECORD, EVI_DERIVED_SIGNAL, };
-UENUM(BlueprintType)
-enum class EReliabilityCategory : uint8 { RLY_DISPROVEN, RLY_UNRELIABLE, RLY_QUESTIONABLE, RLY_RELIABLE, RLY_VERIFIED, };
-
-/// <summary>
-/// Represents one evidence item between two persons of the case
-/// </summary>
-USTRUCT(BlueprintType)
-struct FCaseEvidence {
-	GENERATED_BODY()
-
-	UPROPERTY()
-	int32 id;
-
-	UPROPERTY()
-	EEvidenceType type;
-
-	UPROPERTY()
-	FString name;
-
-	UPROPERTY()
-	FString description;
-
-	UPROPERTY()
-	int32 found_at;
-
-	UPROPERTY()
-	TArray<FEntityRef> linked_to;
-
-	UPROPERTY()
-	int32 origin_event;
-
-	UPROPERTY()
-	EReliabilityCategory reliability;
-
-	UPROPERTY()
-	TArray<FDataPointInfo> matrix_updates;
-};
-// ----------------------------------------------------------------------------
-
-USTRUCT()
-struct FLabResults {
-	GENERATED_BODY()
-};
 
 USTRUCT()
 struct FCaseData {
@@ -71,19 +26,16 @@ struct FCaseData {
 	int32 version;
 };
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEvidenceFound, int32, evidence_idx);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLabRequestComplete, int32, evidence_idx, FLabResults, results);
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTimeBudgetChanged, int32, from, int32, to);
+DECLARE_LOG_CATEGORY_EXTERN(LogCase, Log, Log);
 
 /**
  * Subsystem that will keep track of the current case metadata, resources, player actions on the world.
  * Will store the current status of the case's world.
  * The actions the player take will be reported and handled here
- *   - examine location/evidence
+ *   - examine location/clue
  *   - (start/stop) witness interview
- *   - request study of evidence
- *   - combine info/evidence together
+ *   - request study of clue
+ *   - combine info/clue together
  */
 UCLASS()
 class ALIBI_API UCaseSubsystem : public UGameInstanceSubsystem
@@ -91,27 +43,12 @@ class ALIBI_API UCaseSubsystem : public UGameInstanceSubsystem
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(BlueprintAssignable)
-	FOnEvidenceFound OnEvidenceFound;
-
-	UPROPERTY(BlueprintAssignable)
-	FOnTimeBudgetChanged OnTimeBudgetChanged;
-
 	UPROPERTY(Transient)
 	FCaseData current_case;
 
-	UPROPERTY(Transient)
-	TArray<FCasePerson> case_people;
+	UPROPERTY(SaveGame)
+	int32 actions_used = 0;
 
-	UPROPERTY(Transient)
-	TArray<FCaseLocation> case_locations;
-
-	UPROPERTY(Transient)
-	TArray<FCaseEvent> case_events;
-
-	UPROPERTY(Transient)
-	TArray<FCaseRelation> case_relationship;
-
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	void load_case_file(FString case_path);
 };
